527f2766aca783e022b9763ab369cbafaa25e49d
Reviral <119138081+Reviral@users.noreply.github.com>
2026-05-03 16:07:53 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gantry-v11'; // New version forces the phone to update
+const CACHE_NAME = 'gantry-v12'; // New version forces the phone to update
 const ASSETS = [
   './index.html',
   './manifest.json',
